9a8c41187732b5ff46bc435b67b6807bd048cb10
rkiel <cygnus@pobox.com>
2022-05-20 11:59:47 -0400

    1234: update page title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World</a>
+          Welcome Case Interns</a>
         </h1>
 
         <p className={styles.description}>
